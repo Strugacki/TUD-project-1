18f6d41b48281b9f21a55f1d2990fd02d5a26a85
Strugacki <mhallman94@gmail.com>
2015-10-28 00:14:38 +0100

    added getters and setters

--- skateshop/src/main/java/com/mhallman/skateshop/domain/Client.java
+++ skateshop/src/main/java/com/mhallman/skateshop/domain/Client.java
@@ -12,15 +12,83 @@ public class Client {
 	private long phone_number;
 	
 	
+
+	/**
+	 * Basic constructor for Client class
+	 */
 	public Client(){
 		
 	}
 	
+	/**
+	 * Advanced constructor for Client class
+	 * @param first_name
+	 * @param second_name
+	 * @param phone_nuber
+	 */
 	public Client(String first_name, String second_name, long phone_nuber){
 		super();
-		this.first_name=first_name;
-		this.second_name=second_name;
+		this.setFirst_name(first_name);
+		this.setSecond_name(second_name);
 		this.phone_number=phone_number;
+	}
+
+	/**
+	 * @return the id_client
+	 */
+	public long getId_client() {
+		return id_client;
+	}
+
+	/**
+	 * @param id_client the id_client to set
+	 */
+	public void setId_client(long id_client) {
+		this.id_client = id_client;
+	}
+
+	/**
+	 * @return the first_name
+	 */
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	/**
+	 * @param first_name the first_name to set
+	 */
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	/**
+	 * @return the second_name
+	 */
+	public String getSecond_name() {
+		return second_name;
+	}
+
+	/**
+	 * @param second_name the second_name to set
+	 */
+	public void setSecond_name(String second_name) {
+		this.second_name = second_name;
+	}
+	
+	
+	/**
+	 * @return the phone_number
+	 */
+	public long getPhone_number() {
+		return phone_number;
+	}
+
+	
+	/**
+	 * @param phone_number the phone_number to set
+	 */
+	public void setPhone_number(long phone_number) {
+		this.phone_number = phone_number;
 	}
 	
 	
--- skateshop/src/main/java/com/mhallman/skateshop/domain/Product.java
+++ skateshop/src/main/java/com/mhallman/skateshop/domain/Product.java
@@ -26,9 +26,65 @@ public class Product {
 	 */
 	public Product(String product_name, String brand_name, double price){
 		super();
-		this.product_name=product_name;
-		this.brand_name=brand_name;
-		this.price=price;
+		this.setProduct_name(product_name);
+		this.setBrand_name(brand_name);
+		this.setPrice(price);
+	}
+
+	/**
+	 * @return the id_product
+	 */
+	public long getId_product() {
+		return id_product;
+	}
+
+	/**
+	 * @param id_product the id_product to set
+	 */
+	public void setId_product(long id_product) {
+		this.id_product = id_product;
+	}
+
+	/**
+	 * @return the product_name
+	 */
+	public String getProduct_name() {
+		return product_name;
+	}
+
+	/**
+	 * @param product_name the product_name to set
+	 */
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+	/**
+	 * @return the brand_name
+	 */
+	public String getBrand_name() {
+		return brand_name;
+	}
+
+	/**
+	 * @param brand_name the brand_name to set
+	 */
+	public void setBrand_name(String brand_name) {
+		this.brand_name = brand_name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 	
