1e1231f20baf6797bac6a040bee4c0d0535426fe
Strugacki <mhallman94@gmail.com>
2015-10-28 09:37:04 +0100

    added some managers

--- skateshop/src/main/java/com/mhallman/skateshop/domain/ProductPurchase.java
+++ skateshop/src/main/java/com/mhallman/skateshop/domain/ProductPurchase.java
@@ -2,4 +2,25 @@ package com.mhallman.skateshop.domain;
 
 public class ProductPurchase {
 
+	/**
+	 * Initialization of variables used in ProductPurchase class
+	 */
+	private long id_product;
+	private long id_purchase;
+	private int quantity=0;
+	private double summary=0;
+	
+	
+	public ProductPurchase(){
+		
+	}
+	
+	public ProductPurchase(long id_product, long id_purchase, int quantity, double summary){
+		this.id_product=id_product;
+		this.id_purchase=id_purchase;
+		this.quantity=quantity;
+		this.summary=summary;
+	}
+	
+	
 }
