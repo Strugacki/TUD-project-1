00380561e9a5e10736aa7447122213871bd458da
Strugacki <mhallman94@gmail.com>
2015-10-28 09:08:15 +0100

    Connection class changed to dbConnect
    there was problem with namespaces

---
+++ skateshop/src/main/java/com/mhallman/skateshop/service/dbConnect.java
@@ -0,0 +1,35 @@
+package com.mhallman.skateshop.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnect {
+
+	/**
+	 * Initialization of variables used in dbConnect class
+	 */
+	private Connection conn;
+	private String url = "jdbc:hsqldb:hsql://localhost/workdb";
+	
+	/**
+	 * Basic constructor for dbConnect class
+	 */
+	public dbConnect(){
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			System.out.println("Connection problem appeared!");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * @return
+	 */
+	public Connection getConnection(){
+		return conn;
+	}
+	
+	
+}
