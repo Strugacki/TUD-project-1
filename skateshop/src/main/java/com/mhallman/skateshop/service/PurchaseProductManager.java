449f1d96e64fdf0c73675104feaea32abce8866c
Strugacki <mhallman94@gmail.com>
2015-10-31 12:48:52 +0100

    break

--- skateshop/src/main/java/com/mhallman/skateshop/service/PurchaseProductManager.java
+++ skateshop/src/main/java/com/mhallman/skateshop/service/PurchaseProductManager.java
@@ -1,5 +1,78 @@
 package com.mhallman.skateshop.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PurchaseProductManager {
 
+	
+	/**
+	 * Initialization of variables used in ProductPurchaseManager class
+	 */
+	private PreparedStatement addProductPurchaseStmt;
+	private PreparedStatement deleteProductPurchaseStmt;
+	private PreparedStatement deleteAllProductPurchasesStmt;
+	private PreparedStatement deleteAllProductPurchasesOfProductStmt;
+	private PreparedStatement deleteAllProductPurchasesOfPurchaseStmt;
+	private PreparedStatement getAllProductPurchasesStmt;
+	private PreparedStatement getAllProductPurchasesOfProductStmt;
+	private PreparedStatement getAllProductPurchasesOfPurchaseStmt;
+	private dbConnect dbconn = new dbConnect();
+	private Connection conn = dbconn.getConnection();
+	private Statement statement;
+	private Statement statement2;
+	private Statement statement3;
+	private ResultSet rs;
+	private String alterTableProductPurchase = "ALTER TABLE ProductPurchase ADD CONSTRAINT id_productfk FOREIGN KEY (id_product) REFERENCES Product(id_product) ON DELETE CASCADE";
+	private String alterTableProductPurchase2 = "ALTER TABLE ProductPurchase ADD CONSTRAINT id_purchasefk FOREIGN KEY (id_purchase) REFERENCES Purchase(id_purchase) ON DELETE CASCADE";
+	private String createTableProductPurchase = "CREATE TABLE ProductPurchase(id_purchase bigint, id_product bigint, quantity int,summary double)";
+	
+	
+	/**
+	 * Basic constructor for ProductPurchaseManager class
+	 * - creates table Product if not exists
+	 * - prepares statements 
+	 */
+	public PurchaseProductManager(){
+		try {
+			if(conn!=null){
+				System.out.print("Connected to database\n");
+			}
+			rs = conn.getMetaData().getTables(null, null, null,null);
+			boolean tableExists = false;
+			while (rs.next()) {
+				if ("ProductPurchase".equalsIgnoreCase(rs.getString("TABLE_NAME"))) {
+					tableExists = true;
+					break;
+				}
+			}
+			
+			if (!tableExists){
+				statement = conn.createStatement();
+				statement2 = conn.createStatement();
+				statement3 = conn.createStatement();
+				statement.executeUpdate(createTableProductPurchase);
+				statement2.executeUpdate(alterTableProductPurchase);
+				statement3.executeUpdate(alterTableProductPurchase2);
+			}
+
+			addProductPurchaseStmt = conn.prepareStatement("INSERT INTO ProductPurchase (id_purchase,id_product,quantity,summary) VALUES (?, ?)");
+			deleteProductPurchaseStmt = conn.prepareStatement("DELETE FROM ProductPurchase WHERE id_client= ?");
+			deleteAllProductPurchasesStmt = conn.prepareStatement("DELETE FROM Purchase");
+			deleteAllProductPurchasesOfProductStmt = conn.prepareStatement("DELETE FROM Purchase WHERE id_client=?");
+			deleteAllProductPurchasesOfPurchaseStmt = conn.prepareStatement("DELETE FROM Purchase WHERE id_client=?");
+			getAllProductPurchasesStmt = conn.prepareStatement("SELECT id_purchase, id_client,date FROM Purchase");
+			getAllProductPurchasesOfProductStmt = conn.prepareStatement("SELECT id_purchase,date FROM Purchase WHERE id_client=?");
+			getAllProductPurchasesOfPurchaseStmt = conn.prepareStatement("SELECT id_purchase,date FROM Purchase WHERE id_client=?");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
--- skateshop/src/test/java/com/mhallman/skateshop/test/ProductPurchaseManagerTest.java
+++ skateshop/src/test/java/com/mhallman/skateshop/test/ProductPurchaseManagerTest.java
@@ -1,5 +1,28 @@
 package com.mhallman.skateshop.test;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.mhallman.skateshop.domain.Purchase;
+import com.mhallman.skateshop.domain.Product;
+import com.mhallman.skateshop.service.ClientManager;
+import com.mhallman.skateshop.service.PurchaseManager;
+import com.mhallman.skateshop.service.ProductManager;
+
 public class ProductPurchaseManagerTest {
 
+	private ProductManager prm = new ProductManager();
+	private PurchaseManager pum = new PurchaseManager();
+	private final static String DATE="06-10-1994";
+	private final static String FIRST_NAME="Adam";
+	private final static String SECOND_NAME="Kowalski";
+	private final long PHONE_NUMBER=782694466;
+	private final static String FIRST_NAME2="Bolek";
+	private final static String SECOND_NAME2="Lolek";
+	private final long PHONE_NUMBER2=504683163;
+	
+	
 }
